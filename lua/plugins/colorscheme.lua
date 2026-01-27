f991eacc90dc5d9a56eaac67d0affd26297c3f17
Jaj <codewithjom@gmail.com>
2026-01-27 15:32:37 +0800

    feat(ui): new colorscheme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,13 @@
 return {
   { "lunarvim/darkplus.nvim", priority = 1000 },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 }
