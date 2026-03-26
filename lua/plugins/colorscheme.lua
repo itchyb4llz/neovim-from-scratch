46471f62e83c9d68af233fafe1de4cfe99b8b038
Jaj Dollesin <codewithjom@gmail.com>
2026-03-26 13:34:53 +0800

    feat: updates.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -21,5 +21,17 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    opts = {
+      variant = "auto", -- auto, main, moon, or dawn
+      dark_variant = "main",
+      dim_inactive_windows = false,
+      extend_background_behind_borders = true,
+
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = true,
+      },
+    },
   },
 }
